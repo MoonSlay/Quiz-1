0a9d6011b4bd374e97e67353fdbaf5cd4f5db770
MISS <MISS@E04>
2024-02-29 20:52:35 +0800

    Add project files.

---
+++ Quiz 1/Controllers/BlogController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quiz_1.Models;
+
+namespace Quiz_1.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index(int year, int month, string slug)
+        {
+            //var blogmodel = new BlogModel();
+
+            //if(slug == "blog1")
+            //{
+                
+            //}
+            //else if (slug == "blog2")
+            //{
+            //    BlogModel
+            //        {
+            //        Content = "Blog1";
+            //    }
+            //}
+            //else if (slug == "blog3")
+            //{
+            //    BlogModel
+            //        {
+            //        Content = "Blog1";
+            //    }
+            //}
+            //else if (slug == "blog4")
+            //{
+            //    BlogModel
+            //        {
+            //        Content = "Blog1";
+            //    }
+            //}
+            //else if (slug == "blog5")
+            //{
+            //    BlogModel
+            //        {
+            //        Content = "Blog1";
+            //    }
+            //}
+            //else
+            //{
+            //    return View();
+            //}
+            //return View(blogmodel);
+
+            return View();
+        }
+
+    }
+}
---
+++ Quiz 1/Models/BlogModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Quiz_1.Models
+{
+    public class BlogModel
+    {
+        public string? Title { get; set; } = string.Empty;
+        public string? Content { get; set; } = string.Empty;
+        public string? Write { get; set; } = string.Empty;
+
+    }
+}
